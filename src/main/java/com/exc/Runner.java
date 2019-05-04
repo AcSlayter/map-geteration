64b0b1a948c61614ae4bd1f64a0d76d316c9a4c8
AcSlayter <aaronbcain@gmail.com>
2019-05-03 23:30:33 -0700

    initil checkin

---
+++ src/main/java/com/exc/Runner.java
@@ -0,0 +1,12 @@
+package com.exc;
+
+/**
+ * Created by aaron on 5/3/2019.
+ */
+@SuppressWarnings("ALL")
+public class Runner {
+    public static boolean main (String[] args) {
+        System.out.println(String.join(", ", args));
+        return true;
+    }
+}
